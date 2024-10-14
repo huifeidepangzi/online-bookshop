e97ac09c421a50e1d61e0f71f62c385986c37f21
yshan <huifeidepangzi@gmail.com>
2024-10-15 10:18:57 +1300

    Add DTO model for user creation
    Add DTO model for transfer data from interface layer to application
    layer

---
+++ src/main/java/com/yi/online_bookshop/dto/user/CreateUserDTO.java
@@ -0,0 +1,37 @@
+package com.yi.online_bookshop.dto.user;
+
+import java.time.LocalDateTime;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class CreateUserDTO {
+
+    @NotBlank(message = "Name is required")
+    @NotNull(message = "Age is required")
+    private String name;
+
+    @Email(message = "Email should be valid", regexp = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$")
+    @NotNull(message = "Email is required")
+    @NotEmpty(message = "Email is required")
+    private String email;
+
+    @NotNull(message = "Age is required")
+    @Min(0)
+    @Max(150)
+    private Integer age;
+
+    @NotNull(message = "ActiveFrom is required in format YYYY-MM-DD")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss")
+    private LocalDateTime activeFrom;
+}
