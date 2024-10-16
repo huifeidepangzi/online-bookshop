c5d765cd51cbb9aade990ff25fd4efe77ed4a72e
yshan <huifeidepangzi@gmail.com>
2024-10-16 23:29:51 +1300

    Add activeFrom and activeTo columns on User model

--- src/main/java/com/yi/online_bookshop/domain/entity/user/User.java
+++ src/main/java/com/yi/online_bookshop/domain/entity/user/User.java
@@ -1,5 +1,7 @@
 package com.yi.online_bookshop.domain.entity.user;
 
+import java.time.LocalDateTime;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -7,6 +9,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Data;
+
 
 @Entity
 @Table(name = "users")
@@ -26,4 +29,9 @@ public class User {
     @Column(nullable = true)
     private Integer age;
 
+    @Column(name = "active_from", nullable = false)
+    private LocalDateTime activeFrom;
+
+    @Column(name = "active_to") // Allows null value by default
+    private LocalDateTime activeTo;
 }
