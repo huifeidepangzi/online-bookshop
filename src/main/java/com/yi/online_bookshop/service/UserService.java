af829b28cb255de3749fc5a49de3e62d02821e70
yshan <huifeidepangzi@gmail.com>
2024-10-17 23:09:55 +1300

    Add AccountEntityFactory class to create account entity from user entity

--- src/main/java/com/yi/online_bookshop/service/UserService.java
+++ src/main/java/com/yi/online_bookshop/service/UserService.java
@@ -4,13 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
-import com.yi.online_bookshop.domain.model.account.AccountDomainFactory;
-import com.yi.online_bookshop.domain.model.account.AccountMapper;
 import com.yi.online_bookshop.domain.model.user.UserDomain;
 import com.yi.online_bookshop.domain.model.user.UserDomainFactory;
 import com.yi.online_bookshop.domain.model.user.UserMapper;
 import com.yi.online_bookshop.dto.user.CreateUserDTO;
 import com.yi.online_bookshop.entity.AccountEntity;
+import com.yi.online_bookshop.entity.AccountEntityFactory;
 import com.yi.online_bookshop.entity.UserEntity;
 import com.yi.online_bookshop.repository.AccountRepository;
 import com.yi.online_bookshop.repository.UserRepository;
@@ -30,9 +29,9 @@ public class UserService {
     public UserDomain createUser(CreateUserDTO createUserDTO) throws DataIntegrityViolationException {
         UserDomain userDomain = UserDomainFactory.createUserFromUserDTO(createUserDTO);
         UserEntity userEntity = UserMapper.DomainToEntity(userDomain);
-        userRepository.save(userEntity);
+        UserEntity savedUserEntity = userRepository.save(userEntity);
 
-        AccountEntity accountEntity = AccountMapper.DomainToEntity(AccountDomainFactory.createForUser(userDomain));
+        AccountEntity accountEntity = AccountEntityFactory.createFromUserEntity(savedUserEntity);
         accountRepository.save(accountEntity);
 
         return userDomain;
