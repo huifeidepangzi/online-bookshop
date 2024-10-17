e2164632aee655978a0c1836e7f5f52ad0047448
yshan <huifeidepangzi@gmail.com>
2024-10-17 23:35:04 +1300

    Rename Payment to PaymentDomain and add a new PaymentENtity

---
+++ src/main/java/com/yi/online_bookshop/domain/model/payment/PaymentDomain.java
@@ -0,0 +1,24 @@
+package com.yi.online_bookshop.domain.model.payment;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import com.yi.online_bookshop.entity.AccountEntity;
+import com.yi.online_bookshop.enums.PaymentType;
+
+import lombok.Data;
+
+@Data
+public class PaymentDomain {
+
+    private Long id;
+
+    private AccountEntity account;
+
+    private PaymentType paymentType;
+
+    private BigDecimal amount;
+
+    private LocalDate paymentDate;
+
+}
